cdc81c41b022469afe642f4194bd6ecca4cdea5b
LXM <Xm-Soft@outlook.com>
2020-02-01 23:06:15 +0800

    修改

---
+++ Model/IProduct_ListRepository.cs
@@ -0,0 +1,10 @@
+using System;
+using ZYL.Core.Model;
+using XmSoft.NetCore.DBContext;
+namespace ZYL.Core.Service.Data
+{
+   public interface  IProduct_ListRepository :IDefaultRepository<Product_List>
+   {
+      
+   }
+}
